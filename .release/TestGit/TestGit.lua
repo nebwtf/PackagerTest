1d3cfaab40100164a46eb242b4c9b6aa5a3554d2
Kyle Buller <bullerk@gmail.com>
2016-02-29 19:01:50 -0600

    Add string replacements

--- .release/TestGit/TestGit.lua
+++ .release/TestGit/TestGit.lua
@@ -20,24 +20,24 @@ Testing string replacements
 	Turns into the last changed date (by UTC) of the file in POSIX timestamp. e.g. 1209663296
 
 
-@project-revision@
+
 	Turns into the highest revision of the entire project in integer form. e.g. 1234
 	Note: does not work for git
-@project-hash@
+a8432d191ab1bfa56d855b11a186e0f93265c6d7
 	Turns into the hash of the entire project in hex form. e.g. 106c634df4b3dd4691bf24e148a23e9af35165ea
 	Note: does not work for svn
-@project-abbreviated-hash@
+a8432d1
 	Turns into the abbreviated hash of the entire project in hex form. e.g. 106c63
 	Note: does not work for svn
-@project-author@
+Kyle Buller
 	Turns into the last author of the entire project. e.g. ckknight
-@project-date-iso@
+2016-03-01T00:36:47+0000
 	Turns into the last changed date (by UTC) of the entire project in ISO 8601. e.g. 2008-05-01T12:34:56Z
-@project-date-integer@
+20160301003647
 	Turns into the last changed date (by UTC) of the entire project in a readable integer fashion. e.g. 2008050123456
-@project-timestamp@
+1456792607
 	Turns into the last changed date (by UTC) of the entire project in POSIX timestamp. e.g. 1209663296
-@project-version@
+a8432d1
 	Turns into an approximate version of the project. The tag name if on a tag, otherwise it's up to the repo.
 	:SVN returns something like "r1234"
 	:Git returns something like "v0.1-873fc1"
@@ -45,14 +45,11 @@ Testing string replacements
 
 --]]
 
---@debug@
+--[===[@debug@
 local debug = true
---@end-debug@
+--@end-debug@]===]
 
 --@alpha@
 local alpha = true
 --@end-alpha@
 
---@do-not-package@
-local this_should_never_be_in_a_package
---@end-do-not-package@
