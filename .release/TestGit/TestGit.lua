07599684eaf6adf8e98d0721ba20b89fa8300b27
Kyle Buller <bullerk@gmail.com>
2016-02-29 19:07:15 -0600

    Simplify string replacements

--- .release/TestGit/TestGit.lua
+++ .release/TestGit/TestGit.lua
@@ -23,21 +23,21 @@ Testing string replacements
 
 	Turns into the highest revision of the entire project in integer form. e.g. 1234
 	Note: does not work for git
-a8432d191ab1bfa56d855b11a186e0f93265c6d7
+1d3cfaab40100164a46eb242b4c9b6aa5a3554d2
 	Turns into the hash of the entire project in hex form. e.g. 106c634df4b3dd4691bf24e148a23e9af35165ea
 	Note: does not work for svn
-a8432d1
+1d3cfaa
 	Turns into the abbreviated hash of the entire project in hex form. e.g. 106c63
 	Note: does not work for svn
 Kyle Buller
 	Turns into the last author of the entire project. e.g. ckknight
-2016-03-01T00:36:47+0000
+2016-03-01T01:01:50+0000
 	Turns into the last changed date (by UTC) of the entire project in ISO 8601. e.g. 2008-05-01T12:34:56Z
-20160301003647
+20160301010150
 	Turns into the last changed date (by UTC) of the entire project in a readable integer fashion. e.g. 2008050123456
-1456792607
+1456794110
 	Turns into the last changed date (by UTC) of the entire project in POSIX timestamp. e.g. 1209663296
-a8432d1
+v1.0
 	Turns into an approximate version of the project. The tag name if on a tag, otherwise it's up to the repo.
 	:SVN returns something like "r1234"
 	:Git returns something like "v0.1-873fc1"
@@ -49,7 +49,7 @@ a8432d1
 local debug = true
 --@end-debug@]===]
 
---@alpha@
+--[===[@alpha@
 local alpha = true
---@end-alpha@
+--@end-alpha@]===]
 
